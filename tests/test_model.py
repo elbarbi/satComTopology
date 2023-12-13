c65ce223584594b1f428ff6f84443e30dbdce9d3
Louis Barbier <louis.barbier@isae-supaero.fr>
2023-12-13 14:28:54 +0100

    Replaced UnitTest by Pytest and updated project toml

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,3 @@
-import unittest
-
 from src.sat_com_model.exception import (
     GroundStationConnectionError,
     InterSatelliteConnectionError,
@@ -16,52 +14,65 @@ from src.sat_com_model.model import (
     Link,
     TopologyObject,
 )
+import pytest
 
 
-class TestLinks(unittest.TestCase):
-    satellite: Satellite
-    ground_station: GroundStation
-    user_terminal = UserTerminal
+def assert_can_establish_connection(
+    link: Link,
+    object_source: TopologyObject,
+    object_destination: TopologyObject,
+):
+    link.connect(object_source, object_destination)
+    assert link.source == object_source and link.destination == object_destination
 
-    gsl_link: GroundStationLink
-    isl_link: InterSatelliteLink
-    user_link: UserTerminalLink
 
-    def setUp(self):
-        self.satellite = Satellite()
-        self.ground_station = GroundStation()
-        self.gsl_link = GroundStationLink()
-        self.user_terminal = UserTerminal()
-        self.isl_link = InterSatelliteLink()
-        self.user_link = UserTerminalLink()
-
-    def assert_can_establish_connection(
-        self,
-        link: Link,
-        object_source: TopologyObject,
-        object_destination: TopologyObject,
-    ):
+def assert_cant_establish_connection(
+    link: Link,
+    expected_type_exception,
+    object_source: TopologyObject,
+    object_destination: TopologyObject,
+):
+    with pytest.raises(expected_type_exception):
         link.connect(object_source, object_destination)
-        self.assertTrue(
-            link.source == object_source and link.destination == object_destination
-        )
 
-    def assert_cant_establish_connection(
-        self,
-        link: Link,
-        expected_type_exception,
-        object_source: TopologyObject,
-        object_destination: TopologyObject,
-    ):
-        with self.assertRaises(expected_type_exception):
-            link.connect(object_source, object_destination)
+    assert link.source is None and link.destination is None
 
-        assert link.source is None and link.destination is None
 
+@pytest.fixture
+def satellite() -> Satellite:
+    return Satellite()
+
+
+@pytest.fixture
+def ground_station() -> GroundStation:
+    return GroundStation()
+
+
+@pytest.fixture
+def user_terminal() -> UserTerminal:
+    return UserTerminal()
+
+
+@pytest.fixture
+def gsl_link() -> GroundStationLink:
+    return GroundStationLink()
+
+
+@pytest.fixture
+def isl_link() -> InterSatelliteLink:
+    return InterSatelliteLink()
+
+
+@pytest.fixture
+def user_link() -> UserTerminalLink:
+    return UserTerminalLink()
+
+
+class TestLinks:
     def test_cant_establish_connection_between_same_object(self):
         single_topology_object = TopologyObject()
         link = Link()
-        self.assert_cant_establish_connection(
+        assert_cant_establish_connection(
             link,
             ConnectionBetweenSameSatelliteForbiddenError,
             single_topology_object,
@@ -69,115 +80,137 @@ class TestLinks(unittest.TestCase):
         )
 
     def test_bi_directional_connection_between_satellite_and_ground_station_using_gsl(
-        self,
+        self, gsl_link: GroundStationLink, ground_station, satellite
     ):
-        self.assert_can_establish_connection(
-            self.gsl_link, self.ground_station, self.satellite
-        )
-        self.assert_can_establish_connection(
-            self.gsl_link, self.satellite, self.ground_station
-        )
+        assert_can_establish_connection(gsl_link, ground_station, satellite)
+        assert_can_establish_connection(gsl_link, satellite, ground_station)
 
-    def test_impossible_connection_between_satellites_using_gsl(self):
+    def test_impossible_connection_between_satellites_using_gsl(
+        self, gsl_link: GroundStationLink, satellite
+    ):
         second_satellite = Satellite()
-        self.assert_cant_establish_connection(
-            self.gsl_link,
+        assert_cant_establish_connection(
+            gsl_link,
             GroundStationConnectionError,
-            self.satellite,
+            satellite,
             second_satellite,
         )
 
-    def test_impossible_connection_between_satellite_and_user_using_gsl(self):
-        self.assert_cant_establish_connection(
-            self.gsl_link,
+    def test_impossible_connection_between_satellite_and_user_using_gsl(
+        self,
+        gsl_link: GroundStationLink,
+        satellite: Satellite,
+        user_terminal: UserTerminal,
+    ):
+        assert_cant_establish_connection(
+            gsl_link,
             GroundStationConnectionError,
-            self.satellite,
-            self.user_terminal,
+            satellite,
+            user_terminal,
         )
-        self.assert_cant_establish_connection(
-            self.gsl_link,
+        assert_cant_establish_connection(
+            gsl_link,
             GroundStationConnectionError,
-            self.user_terminal,
-            self.satellite,
+            user_terminal,
+            satellite,
         )
 
-    def test_satellites_cannot_connect_using_gsl_link(self):
+    def test_satellites_cannot_connect_using_gsl_link(
+        self, gsl_link: GroundStationLink, satellite: Satellite
+    ):
         other_satellite = Satellite()
 
-        self.assert_cant_establish_connection(
-            self.gsl_link, GroundStationConnectionError, self.satellite, other_satellite
+        assert_cant_establish_connection(
+            gsl_link, GroundStationConnectionError, satellite, other_satellite
         )
 
-    def test_user_and_satellite_cannot_connect_using_gsl_link(self):
-        self.assert_cant_establish_connection(
-            self.gsl_link,
+    def test_user_and_satellite_cannot_connect_using_gsl_link(
+        self,
+        gsl_link: GroundStationLink,
+        satellite: Satellite,
+        user_terminal: UserTerminal,
+    ):
+        assert_cant_establish_connection(
+            gsl_link,
             GroundStationConnectionError,
-            self.satellite,
-            self.user_terminal,
+            satellite,
+            user_terminal,
         )
-        self.assert_cant_establish_connection(
-            self.gsl_link,
+        assert_cant_establish_connection(
+            gsl_link,
             GroundStationConnectionError,
-            self.user_terminal,
-            self.satellite,
+            user_terminal,
+            satellite,
         )
 
-    def test_satellite_can_connect_to_satellite_using_isl(self):
+    def test_satellite_can_connect_to_satellite_using_isl(
+        self, isl_link: InterSatelliteLink, satellite: Satellite
+    ):
         other_satellite = Satellite()
-        self.assert_can_establish_connection(
-            self.isl_link, self.satellite, other_satellite
+        assert_can_establish_connection(isl_link, satellite, other_satellite)
+
+    def test_satellite_cant_connect_to_gs_with_isl(
+        self,
+        isl_link: InterSatelliteLink,
+        satellite: Satellite,
+        ground_station: GroundStation,
+    ):
+        assert_cant_establish_connection(
+            isl_link,
+            InterSatelliteConnectionError,
+            satellite,
+            ground_station,
+        )
+        assert_cant_establish_connection(
+            isl_link,
+            InterSatelliteConnectionError,
+            ground_station,
+            satellite,
         )
 
-    def test_satellite_cant_connect_to_gs_with_isl(self):
-        self.assert_cant_establish_connection(
-            self.isl_link,
+    def test_satellite_cant_connect_to_user_with_isl(
+        self,
+        isl_link: InterSatelliteLink,
+        satellite: Satellite,
+        user_terminal: UserTerminal,
+    ):
+        assert_cant_establish_connection(
+            isl_link,
             InterSatelliteConnectionError,
-            self.satellite,
-            self.ground_station,
+            satellite,
+            user_terminal,
         )
-        self.assert_cant_establish_connection(
-            self.isl_link,
+        assert_cant_establish_connection(
+            isl_link,
             InterSatelliteConnectionError,
-            self.ground_station,
-            self.satellite,
+            satellite,
+            user_terminal,
         )
 
-    def test_satellite_cant_connect_to_user_with_isl(self):
-        self.assert_cant_establish_connection(
-            self.isl_link,
-            InterSatelliteConnectionError,
-            self.satellite,
-            self.user_terminal,
-        )
-        self.assert_cant_establish_connection(
-            self.isl_link,
-            InterSatelliteConnectionError,
-            self.satellite,
-            self.user_terminal,
-        )
+    def test_user_connect_with_satellite_using_utl(
+        self,
+        user_link: UserTerminalLink,
+        user_terminal: UserTerminal,
+        satellite: Satellite,
+    ):
+        assert_can_establish_connection(user_link, user_terminal, satellite)
+        assert_can_establish_connection(user_link, satellite, user_terminal)
 
-    def test_user_connect_with_satellite_using_utl(self):
-        self.assert_can_establish_connection(
-            self.user_link, self.user_terminal, self.satellite
-        )
-        self.assert_can_establish_connection(
-            self.user_link, self.satellite, self.user_terminal
-        )
-
-    def test_gs_cant_connect_with_satellite_utl(self):
-        self.assert_cant_establish_connection(
-            self.user_link,
+    def test_gs_cant_connect_with_satellite_utl(
+        self,
+        user_link: UserTerminalLink,
+        ground_station: GroundStation,
+        satellite: Satellite,
+    ):
+        assert_cant_establish_connection(
+            user_link,
             UserTerminalConnectionError,
-            self.ground_station,
-            self.satellite,
+            ground_station,
+            satellite,
         )
-        self.assert_cant_establish_connection(
-            self.user_link,
+        assert_cant_establish_connection(
+            user_link,
             UserTerminalConnectionError,
-            self.satellite,
-            self.ground_station,
+            satellite,
+            ground_station,
         )
-
-
-if __name__ == "__main__":
-    unittest.main()
