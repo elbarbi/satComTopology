0d3a99fe3745c33fbcf1e3ee32564d83cc629674
Louis Barbier <louis.barbier@isae-supaero.fr>
2023-12-19 13:58:12 +0100

    Fixed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="satComTopology",
+    name="sat_com_topology",
     version="1.0",
     packages=find_packages(include=["sat_com_model", "sat_com_model.*"]),
     install_requires=[],
