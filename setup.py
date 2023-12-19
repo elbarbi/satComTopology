73b090832a672bb3ded2994e7c01c400385dcb08
Louis Barbier <louis.barbier@isae-supaero.fr>
2023-12-19 14:25:01 +0100

    Removed not mandatory code

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="sat_com_topology",
     version="1.0",
-    packages=find_packages(
-        include=["sat_com_topology.sat_com_model", "sat_com_topology.sat_com_model.*"]
-    ),
+    packages=find_packages(),
     install_requires=[],
 )
