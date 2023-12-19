57aac308e53954d953751e454c6ca527f9fc666b
Louis Barbier <louis.barbier@isae-supaero.fr>
2023-12-19 13:49:19 +0100

    Replaced hatch to setuptools

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="satComTopology",
     version="1.0",
-    packages=find_packages(),
+    packages=find_packages(include=["sat_com_model", "sat_com_model.*"]),
     install_requires=[],
 )
