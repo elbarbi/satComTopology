48cc7f15a90309ea6eddf3f7e11c912f3de7c15a
Louis Barbier <louis.barbier@isae-supaero.fr>
2023-12-19 14:03:53 +0100

    Fixed include packages

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="sat_com_topology",
     version="1.0",
-    packages=find_packages(include=["sat_com_model", "sat_com_model.*"]),
+    packages=find_packages(
+        include=["satComTopology.sat_com_model", "satComTopology.sat_com_model.*"]
+    ),
     install_requires=[],
 )
